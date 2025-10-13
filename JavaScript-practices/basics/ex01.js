98552b2e953d51810aeaa225786b1e37ba20e996
Yuna-commits <ksm13563661@gmail.com>
2025-10-13 11:16:10 +0900

    first commit

---
+++ JavaScript-practices/basics/ex01.js
@@ -0,0 +1,14 @@
+console.log("===== [01] 기본타입, Primitive type");
+var u; // undefined
+var i = 10; // number
+var s = "hello world"; // string
+var b = true; // boolean
+
+console.log("u: " + typeof u);
+console.log("i: " + typeof i);
+console.log("s: " + typeof s);
+console.log("b: " + typeof b);
+
+// console.log(k); // k is not defined
+
+console.log("===== [02] 객체타입, Object type");
